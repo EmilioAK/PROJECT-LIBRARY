f23d1296c7c3f6e29f3054a534211759dd65ea58
EmilioAK <emilio.alvarez96@gmail.com>
2020-12-20 15:45:35 +0100

    toggle_read

--- app.js
+++ app.js
@@ -7,14 +7,6 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
-function removeBook (title, library) {
-    for (let book of library) {
-        if (book.title === title) {
-            library.splice(library.indexOf(book), 1)
-        }
-    }
-}
-
 function createAndAppendElement(htmlType, htmlClass, elementToAppendTo, textContent = "") {
     const ELEMENT = document.createElement(htmlType);
     ELEMENT.classList.add(htmlClass);
@@ -23,26 +15,45 @@ function createAndAppendElement(htmlType, htmlClass, elementToAppendTo, textCont
     return ELEMENT;
 }
 
-function createBookCard (title, author, pages, read) {
+function createBookCard (title, author, pages, read, bookIndex = 0) {
     const BOOKLIST = document.getElementById("bookList");
     const BOOKCARD = createAndAppendElement("div", "bookCard", BOOKLIST);
+    BOOKCARD.setAttribute("data-bookIndex", bookIndex);
 
     const TITLE = createAndAppendElement("div", "bookTitle", BOOKCARD, title);
     const AUTHOR = createAndAppendElement("div", "bookAuthor", BOOKCARD, author);
     const PAGES = createAndAppendElement("div", "bookPages", BOOKCARD, pages);
     const READ = createAndAppendElement("div", "bookRead", BOOKCARD, read);
+    READ.checked = read;
     
     const DELETE_BUTTON = createAndAppendElement("button", "deleteButton", BOOKCARD, "Delete book");
     DELETE_BUTTON.addEventListener("click", () => {
-        removeBook(title, myLibrary);
-        updatePageLibrary(myLibrary)
+        myLibrary.splice(bookIndex, 1);
+        updatePageLibrary(myLibrary);
     })
+    const TOGGLE_READ = createAndAppendElement("input", "toggleRead", BOOKCARD);
+    TOGGLE_READ.setAttribute("type", "checkbox");
+    TOGGLE_READ.addEventListener('change', function() {
+        if (this.checked) {
+            myLibrary[bookIndex].read = true;
+            console.log(myLibrary[bookIndex].read);
+            console.log(READ.checked);
+            updatePageLibrary(myLibrary);
+        } else {
+            myLibrary[bookIndex].read = false;
+            console.log(myLibrary[bookIndex].read);
+            console.log(READ.checked);
+            updatePageLibrary(myLibrary);
+        }
+      });
 }
 
 function updatePageLibrary (library) {
     document.querySelector("#bookList").innerHTML = ""; //Clears it on each call so it doesn't keep old values but just shows the current list
-    for (book of library) {
-        createBookCard(book.title, book.author, book.pages, book.read);
+    
+    for (let book of library) {
+        createBookCard(book.title, book.author, book.pages, book.read, library.indexOf(book));
+        console.log(library.indexOf(book), book)
     }
 }
 

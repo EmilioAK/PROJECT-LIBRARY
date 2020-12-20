4f2b9aa8a1ce077d2c45239648849d060b0ed027
EmilioAK <emilio.alvarez96@gmail.com>
2020-12-20 04:49:52 +0100

    Add bookremoval

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+function removeBook (title, library) {
+    for (let book of library) {
+        if (book.title === title) {
+            library.splice(library.indexOf(book), 1)
+        }
+    }
+}
+
 function createAndAddElement(htmlType, htmlClass, elementToAppendTo, textContent = "") {
     const ELEMENT = document.createElement(htmlType);
     ELEMENT.classList.add(htmlClass);
@@ -23,21 +31,27 @@ function createBookCard (title, author, pages, read) {
     const AUTHOR = createAndAddElement("div", "bookAuthor", BOOKCARD, author);
     const PAGES = createAndAddElement("div", "bookPages", BOOKCARD, pages);
     const READ = createAndAddElement("div", "bookRead", BOOKCARD, read);
+    
+    const DELETE_BUTTON = createAndAddElement("button", "deleteButton", BOOKCARD, "Delete book");
+    DELETE_BUTTON.addEventListener("click", () => {
+        removeBook(title, myLibrary);
+        updatePageLibrary(myLibrary)
+    })
 }
 
-function addLibraryToPage (library) {
+function updatePageLibrary (library) {
     document.querySelector("#bookList").innerHTML = ""; //Clears it on each call so it doesn't keep old values but just shows the current list
     for (book of library) {
         createBookCard(book.title, book.author, book.pages, book.read);
     }
 }
 
-function addBookToLibrary() {
+function addBookToLibraryFromPage() {
     const TITLE = document.getElementById("userTitle").value;
     const AUTHOR = document.getElementById("userAuthor").value;
     const PAGES = document.getElementById("userPages").value;
     myLibrary.push(new Book(TITLE, AUTHOR, PAGES, true));
-    addLibraryToPage(myLibrary);
+    updatePageLibrary(myLibrary);
 }
 
-document.getElementById("newBook").addEventListener("click", addBookToLibrary)
+document.getElementById("newBook").addEventListener("click", addBookToLibraryFromPage)
